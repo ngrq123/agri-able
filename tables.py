d5f2db0697d498e30d962ebac19f590004ee3bde
unknown <teominsi@hotmail.com>
2022-08-25 01:16:27 +0800

    added form

--- tables.py
+++ tables.py
@@ -1,8 +1,16 @@
 import streamlit as st
 import pandas as pd
 
+st.set_page_config(
+     page_title="ASDI",
+     page_icon="🧊",
+     layout="wide",
+     initial_sidebar_state="expanded",
+     
+ )
+
 st.write ("""
-# Table Recommender
+# Recommender
 """)
 
 d = pd.read_csv ("crop_attributes.csv")
@@ -14,11 +22,30 @@ df = pd.DataFrame(data=d)
 # st.write(df)
 
 # Now this will show the filtered row in the dataframe as you change the inputs
+
+with st.form("my_form"):
+    st.write("Enter your soil details to get a recommendation:")
+    soil_ph = st.number_input('Enter the soil PH', key=1)
+    potassium = st.number_input('Enter the potassium level', key=2)
+    nitrogen = st.number_input('Enter the nitrogen level', key=3)
+
+
+
+
+    submitted = st.form_submit_button("Submit your crop information")
+    if submitted:
+        st.write("soil ph -", soil_ph)
+        st.write("potassium -", potassium)
+        st.write("nitrogen -", nitrogen)
+
+
 min_ph_soil = st.selectbox('Min PH', df['min_ph_soil'].unique())
+max_ph_soil = st.selectbox('Max PH', df['max_ph_soil'].unique())
+
+
 
 
 newDF = (df[df['min_ph_soil'] >= min_ph_soil])
 
-max_ph_soil = st.selectbox('Max PH', df['max_ph_soil'].unique())
 
 st.write(newDF[newDF['max_ph_soil'] <= max_ph_soil])
